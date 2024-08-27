a6dab2074b40c9a4af840aa0175ff30b37e90ba4
masdefry <muhammaddefryan@gmail.com>
2024-08-27 11:44:36 +0700

    Increment Decrement

---
+++ operator.js
@@ -0,0 +1,20 @@
+// >>>>> OPERATOR <<<<<
+// +, -, /, *, %
+console.log(1 + 1) // Penjumlahan
+console.log(1 - 1) // Pengurangan
+console.log(1 / 1) // Pembagian
+console.log(1 * 1) // Perkalian
+console.log(5 % 2) // 1 - Modulus: Sisa pembagian
+console.log(10 % 2) // 0 - Modulus: Sisa pembagian
+console.log(10 % 6) // 4 - Modulus: Sisa pembagian
+
+
+// String Concate: Apapun yang dijumlahkan dengan string, maka akan di join 
+console.log('10' + 10) // 1010
+console.log('Defryan' + 100) // Defryan100
+console.log('Purwadhika' + 'Schooll') // PurwadhikaSchool
+
+console.log('10' * 10)  
+console.log('10' - 10) 
+console.log('10' * 'Abc') // NaN(Not a Number)
+console.log('10' * true) // 10
