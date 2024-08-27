7a716363066b030f6408fa563276ff392efebfbd
masdefry <muhammaddefryan@gmail.com>
2024-08-27 11:15:43 +0700

    Data Type

---
+++ data.types.js
@@ -0,0 +1,31 @@
+// >>>>> DATA TYPES <<<<<
+// Dibagi menjadi 2: Primitive & Non Primitive
+
+// >>>>> Primitive Data Type <<<<<
+// String: Valuenya dibungkus dengan '', "", ``
+const text = 'Lorem ipsum'
+const paragraph = "Hello, I'm JCWD-3002"
+const greeting = `Hi, User01!`
+console.log(typeof text)
+console.log(typeof paragraph)
+console.log(typeof greeting)
+
+const programCode = '3002'
+console.log(typeof programCode)
+
+// Number
+const discount = 10
+console.log(typeof discount)
+
+// Boolean: Tipe data yang memiliki 2 value (true or false)
+const isMarried = false
+const isGraduated = true 
+console.log(typeof isMarried)
+console.log(typeof isGraduated)
+
+// Null & Undefined 
+let newStudent
+let studentBSD = null
+
+console.log(typeof newStudent)
+console.log(typeof studentBSD)
