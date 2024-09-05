6dfaa99c0aab2985c5057086f6e57a7d22567144
masdefry <muhammaddefryan@gmail.com>
2024-09-05 09:13:28 +0700

    Day07 Done:

--- exercise.js
+++ exercise.js
@@ -1,179 +1,47 @@
-// Calculate Student
-class Student{
-    constructor(name, email, age, score){
-        this.name = name; 
-        this.email = email; 
-        this.age = age; 
-        this.score = score
+// 01. Function to Check Two Property of Object Are Equal
+// Ex. { a: 1 } & { a: 100 } ---> FALSE
+//     { a: 1 } & { a: 1 } ---> TRUE
+
+// 02. Function to Get Intersection of Two Object
+// Ex. { a: 1, b: 2 } & { a: 1, c: 3 } ---> { a: 1 }
+function IntersectionObj(obj1, obj2){
+    let newObj = {}
+
+    for(let key in obj1){
+        if(obj1[key] === obj2[key]){
+            newObj[key] = obj1[key ]
+        }
     }
 
-    convertAge(){
-        return new Date().getFullYear() - new Date(this.age).getFullYear()
-    }
+    console.log(newObj)
 }
 
-const students = [
-    new Student('Defryan', 'defryan@gmail.com', '2000-01-01', 90),
-    new Student('Aviawan', 'aviawan@menkominfo.com', '2000-01-01', 100), 
-    new Student('Yusuf', 'yusuf@yahoo.com', '2000-01-01', 95)
-]
+let obj1 = {
+    a: 1, 
+    c: 2
+}
+let obj2 = {
+    c: 3,
+    a: 1
+}
+IntersectionObj(
+    obj1, obj2
+)
 
-function CalculateStudentData(arrStudents){
-    const arrScore = []
-    const arrAge = []
+// 03. Function to Switch Key & Value 
+// Ex. [ { name: 'David', age: 20 } ] ---> [ { David: 'name', 20: 'age' } ]
+
+// 04. Recursive Function
+function RecursiveFactorial(number, currentValue = 1){ // number = 3; currentValue = 20
+    currentValue *= number // 20 *= 3 = 60
+    number-- // number = 2
+
+    if(number > 0){ // 4>0? T; 3>0? T; 2>0? T
+        RecursiveFactorial(number, currentValue) // 2, 60
+    }else{
+        console.log(currentValue)
+    }
     
-    arrStudents.forEach(item => {
-        arrScore.push(item.score)
-        arrAge.push(item.convertAge())
-    })
-
-    return {
-        score: {
-            min: Math.min(...arrScore), 
-            max: Math.max(...arrScore),
-            avg: arrScore.reduce((acc, currentValue) => acc + currentValue)/arrScore.length
-        },
-        age: {
-            min: Math.min(...arrAge), 
-            max: Math.max(...arrAge),
-            avg: arrAge.reduce((acc, currentValue) => acc + currentValue)/arrAge.length
-        }
-    }
 }
 
-console.log(CalculateStudentData(students))
-
-
-
-// Transaction
-class Products{
-    constructor(name, price){
-        this.name = name; 
-        this.price = price;
-    }
-}
-
-const products = [
-    new Products('Sepatu', 150000), 
-    new Products('Kaos', 100000), 
-    new Products('Celana', 100000)
-]
-
-class Transaction{
-    total = 0;
-    carts = [];
-
-    addToCart(selectedProduct, qty){
-        let indexFindItem
-        
-        // Update Total
-        this.total += selectedProduct.price * qty 
-
-        if(this.carts.length === 0){
-            this.carts.push({...selectedProduct, qty})
-        }else{
-            this.carts.forEach((item, index) => {
-                if(item.name === selectedProduct.name){
-                    indexFindItem = index
-                }
-            })  
-
-            if(indexFindItem !== undefined){ // Apabila Item yang dicari ADA
-                this.carts[indexFindItem].qty += qty
-            }else if(indexFindItem === undefined){ // Apabila Item yang dicari TIDAK ADA
-                this.carts.push({...selectedProduct, qty})
-            }
-        }
-    } 
-
-    showCarts(){
-        return this.carts
-    }
-
-    showTotal(){
-        return this.total
-    }
-}
-
-const newTransaction = new Transaction()
-newTransaction.addToCart(products[0], 1)
-newTransaction.addToCart(products[1], 1)
-// console.log(newTransaction.carts)
-console.log(newTransaction.showCarts())
-console.log(newTransaction.showTotal())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const arrProducts = [
-//     {
-//         name: 'Sepatu', 
-//         price: 1000000
-//     }, 
-//     {
-//         name: 'Kaos', 
-//         price: 50000
-//     }
-// ]
-
-// // const carts = []
-
-
-
-// // ERROR-01: Tidak Mau Me-Looping, Karena carts Itu Kosong
-// carts.forEach(item => {
-//     carts.push(arrProducts[0])
-// })
-
-// // SOLVE
-// if(carts.length === 0){
-//     carts.push(arrProducts[0])
-// }else{
-//     // Lakukan Looping
-// }
-
-// // ERROR-02: Kita Mau Menambahkan Sepatu ke Dalam Carts. Tetapi di Dalam Carts Itu Sudah Ada Produk Sepatu dan Kaos
-// const carts = [
-//     {
-//         name: 'Sepatu', 
-//         price: 1000000,
-//         qty: 2
-//     }, 
-//     {
-//         name: 'Kaos', 
-//         price: 50000, 
-//         qty: 1
-//     }, 
-//     {
-//         name: 'Sepatu', 
-//         price: 1000000, 
-//         qty: 1
-//     }
-// ]
-
-// if(carts.length === 0){
-//     carts.push(arrProducts[0])
-// }else{
-//     // Lakukan Looping
-//     carts.forEach(item => {
-//         if(item.name === arrProducts[0].name){
-//             item.qty += 1
-//         }else{
-//             carts.push(arrProducts[0])
-//         }
-//     })
-// }
+RecursiveFactorial(5)
